0ff917e3fa0aa207864a7b43d0987087b1adaff9
Joshua Correia <joshua.correia@Dexcom.com>
2019-08-04 12:16:14 -0700

    Added total_spent

--- README.md
+++ README.md
@@ -11,6 +11,9 @@ How to configure this tool:
 If you wish to track stocks, edit my_stocks.txt
 If you wish to track cryptocurrency, edit my_crypto.txt
 Both will be configured into your total gains/losses if you have values in both files
+If you bought the same stock at multiple price points, you can add the price points on different lines like so:
+AAPL, 100, 25
+AAPL, 200, 5
 
 How to run this tool:
 Open up a terminal and type 'python3 track.py' and that's it!
--- track.py
+++ track.py
@@ -6,13 +6,15 @@ GREEN = '\033[32m'
 RED = '\033[31m'
 
 overall_gains = 0
+total_spent = 0
 
 
 def main():
     parse_crypto_file()
     parse_stocks_file()
 
-    print_colored_price("Overall gains:", overall_gains)
+    print(f"Total spent: ${total_spent}")
+    print_colored_price("OVERALL GAINS:", overall_gains)
 
 
 def parse_stocks_file():
@@ -43,18 +45,22 @@ def parse_crypto_file():
             else:
                 line = line.replace(" ", "").replace("\n", "").split(",")
                 crypto_name = line[0]
-                purchase_price = float(line[1])
+                purchase_price_per_share = float(line[1])
                 num_shares = float(line[2])
                 current_price = get_current_crypto_price(crypto_name)
-                gain = current_price - purchase_price
+                gain = current_price - purchase_price_per_share
                 total_gain = gain*num_shares
 
                 global overall_gains
                 overall_gains += total_gain
 
+                global total_spent
+                total_spent += purchase_price_per_share*num_shares
+
                 print(f"Cryptocurrency: {crypto_name}")
-                print(f"Purchase price: ${format(purchase_price, '.2f')}")
-                print(f"Current price: ${format(current_price, '.2f')}")
+                print(f"Number of shares: {num_shares}")
+                print(f"Purchase price per share: ${format(purchase_price_per_share, '.2f')}")
+                print(f"Current price per share: ${format(current_price, '.2f')}")
                 print_colored_price("Gain per share:", gain)
                 print_colored_price("Total gain for this cryptocurrency:", total_gain)
                 print()
@@ -74,21 +80,25 @@ def print_colored_price(message, value):
 def parse_stock(line):
     try:
         stock_name = line[0]
-        purchase_price = float(line[1])
+        purchase_price_per_share = float(line[1])
         num_shares = float(line[2])
         current_price = yf.Ticker(stock_name).info['regularMarketPrice']
-        gain = current_price - purchase_price
+        gain = current_price - purchase_price_per_share
         total_gain = gain*num_shares
 
         print(f"Stock: {stock_name}")
-        print(f"Purchase price: ${format(purchase_price, '.2f')}")
-        print(f"Current price: ${format(current_price, '.2f')}")
+        print(f"Number of shares: {num_shares}")
+        print(f"Purchase price per share: ${format(purchase_price_per_share, '.2f')}")
+        print(f"Current price per share: ${format(current_price, '.2f')}")
         print_colored_price("Gain per share:", gain)
         print_colored_price("Total gain for this stock:", total_gain)
         print()
 
         global overall_gains
         overall_gains += total_gain
+
+        global total_spent
+        total_spent += purchase_price_per_share*num_shares
     except KeyError:
         print(f"ERROR: Unable to find a stock with the name {stock_name}!")
 
